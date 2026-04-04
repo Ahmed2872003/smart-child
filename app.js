4c4f9aeb824edfc1f49caa7635c6918474c17ea9
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-04 13:14:02 +0200

    Init repository

---
+++ app.js
@@ -0,0 +1,31 @@
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/error");
+
+const userRouter = require(`${__dirname}/routes/users`);
+
+const setupMiddlewares = require(`${__dirname}/utils/middlewares`);
+
+const express = require("express");
+
+const APIVersion = "1";
+const app = express();
+
+// MIDDLEWARE
+setupMiddlewares(app);
+
+// ROUTES
+app.use(`/api/v${APIVersion}/users`, userRouter);
+
+// Error handler for invalid routes
+app.all("*", (req, res, next) => {
+	const err = new AppError(
+		`can't find ${req.originalUrl} on this server!`,
+		404,
+	);
+
+	next(err);
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
