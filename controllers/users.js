5be0cd9afdd310d7c85ce4b91c9e802d7bf3c228
Mohamed Reda <mohamedredaelsaid0@gmail.com>
2026-04-09 08:45:24 +0200

    fix: remove duplicate methods

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,8 @@
-const User = require("./../models/user");
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError");
-const filterObj = require("./../utils/filterObj");
-const factory = require("./../controllers/handlerFactory");
+const User = require('./../models/user');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const filterObj = require('./../utils/filterObj');
+const factory = require('./../controllers/handlerFactory');
 
 exports.getUser = factory.getOne(User);
 
@@ -13,40 +13,28 @@ exports.getAllUsers = factory.getMany(User);
 exports.updateUser = catchAsync(async (req, res, next) => {
   if (req.body.password || req.body.passwordConfirm) {
     return next(
-      new AppError("This route is not for user password updates.", 400),
+      new AppError('This route is not for user password updates.', 400),
     );
   }
 
-  const filteredBody = filterObj(req.body, "name", "email");
+  const filteredBody = filterObj(req.body, 'name', 'email');
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
     runValidators: true,
     new: true,
   });
 
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
       user: updatedUser,
     },
   });
 });
 
-exports.updateMyInfo = (req, res, next) => {
-  const user = req.user;
-
-  if (req.body.password || req.body.passwordConfirm) {
-    return next(new AppError("You cannot update password!", 400));
-  }
-
-  res.status(200).json({
-    status: "success",
-  });
-};
-
 exports.deleteMyAccount = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
   res.status(204).json({
-    status: "success",
+    status: 'success',
     data: null,
   });
 });
